b7c74846e2c1cd821bfa0daa1f9a23cc5721b9ed
Daniel Pacrami <daniel.pacrami@sap.com>
2020-08-24 14:16:43 -0400

    Update version in url string

--- gardenctl.rb
+++ gardenctl.rb
@@ -4,10 +4,10 @@ class Gardenctl < Formula
   version "v0.21.0"
 
   if OS.mac?
-    url "https://github.com/gardener/gardenctl/releases/download/v0.19.0/gardenctl-darwin-amd64"
+    url "https://github.com/gardener/gardenctl/releases/download/v0.21.0/gardenctl-darwin-amd64"
     sha256 "7b27b54cb14420bc44fe02c08f1b451f10fb4cd8dd266b414e80c8d9b5f5b286"
   elsif OS.linux?
-    url "https://github.com/gardener/gardenctl/releases/download/v0.19.0/gardenctl-linux-amd64"
+    url "https://github.com/gardener/gardenctl/releases/download/v0.21.0/gardenctl-linux-amd64"
     sha256 "32ceb64608523dc166ce9aa0d89c1c4ee299920cf99e0bfe4953a7c12594e524"
   end
 
