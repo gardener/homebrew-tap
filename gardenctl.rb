869c4ca3cf7f5854854e7fc0c884c07f1146dd7c
gardener-robot-ci-1 <gardener.ci.user@gmail.com>
2020-12-02 02:32:25 +0000

    Update gardenctl

--- gardenctl.rb
+++ gardenctl.rb
@@ -1,14 +1,14 @@
 class Gardenctl < Formula
   desc "Gardenctl"
   homepage "https://gardener.cloud"
-  version "v0.23.1"
+  version "v0.24.0"
 
   if OS.mac?
-    url "https://github.com/gardener/gardenctl/releases/download/v0.23.1/gardenctl-darwin-amd64"
-    sha256 "81cb57c307d53a737e86aebb0b969c9b147ccd0f8fb5409ff9dedd28390b6b6f"
+    url "https://github.com/gardener/gardenctl/releases/download/v0.24.0/gardenctl-darwin-amd64"
+    sha256 "8f7c9c8f2cdfb34b4a17bb540a921729dc69cc4d230ae673d11d8c98c5b2c5c0"
   elsif OS.linux?
-    url "https://github.com/gardener/gardenctl/releases/download/v0.23.1/gardenctl-linux-amd64"
-    sha256 "1b4482627970153c653e87b338472466b4440d63b432a38a1079003f59e1799c"
+    url "https://github.com/gardener/gardenctl/releases/download/v0.24.0/gardenctl-linux-amd64"
+    sha256 "616383077e3039c9932f6baeea6efaaa7763d0232f0646135c34a723f26302e2"
   end
 
   depends_on :arch => :x86_64
