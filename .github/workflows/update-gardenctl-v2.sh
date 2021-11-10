d883b55172d7a1c6bee78536e98c28c36e8f12ad
Lukas Gross <lukas.gross@sap.com>
2021-11-10 12:26:26 +0100

    updated scripts for darwin arm builds

--- .github/workflows/update-gardenctl-v2.sh
+++ .github/workflows/update-gardenctl-v2.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 tag=${1}
-darwin_sha=${2}
-linux_sha=${3}
+darwin_sha_amd64=${2}
+darwin_sha_arm64=${3}
+linux_sha=${4}
 if [ -z "${1}" ]
 then
       echo "release tag is not provided"
@@ -10,28 +11,39 @@ then
 fi
 if [ -z "${2}" ]
 then
-      echo "mac binary sha256sum is not provided"
+      echo "darwin amd64 binary sha256sum is not provided"
       exit 1
 fi
 if [ -z "${3}" ]
+then
+      echo "darwin arm64 binary sha256sum is not provided"
+      exit 1
+fi
+if [ -z "${4}" ]
 then
       echo "linux binary sha256sum is not provided"
       exit 1
 fi
 
 echo $tag
-echo $darwin_sha
+echo $darwin_sha_amd64
+echo $darwin_sha_arm64
 echo $linux_sha
 
 cat > gardenctl-v2.rb << EOF
-class Gardenctl < Formula
+class GardenctlV2 < Formula
   desc "Gardenctl-v2"
   homepage "https://gardener.cloud"
   version "$tag"
 
   if OS.mac?
-    url "https://github.com/gardener/gardenctl-v2/releases/download/$tag/gardenctl_v2_darwin_amd64"
-    sha256 "$darwin_sha"
+    if Hardware::CPU.arm?
+      url "https://github.com/gardener/gardenctl-v2/releases/download/$tag/gardenctl_v2_darwin_arm64"
+      sha256 "$darwin_sha_arm64"
+    else
+      url "https://github.com/gardener/gardenctl-v2/releases/download/$tag/gardenctl_v2_darwin_amd64"
+      sha256 "$darwin_sha_amd64"
+    end
   elsif OS.linux?
     url "https://github.com/gardener/gardenctl-v2/releases/download/$tag/gardenctl_v2_linux_amd64"
     sha256 "$linux_sha"
