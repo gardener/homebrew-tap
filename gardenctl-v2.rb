c9e5e7038b5a70cc8503bb4c54f203dc2272d560
Lukas Gross <35373687+grolu@users.noreply.github.com>
2022-02-25 17:05:51 +0100

    Update gardenctl-v2.rb

--- gardenctl-v2.rb
+++ gardenctl-v2.rb
@@ -14,9 +14,8 @@ class GardenctlV2 < Formula
   elsif OS.linux?
     url "https://github.com/gardener/gardenctl-v2/releases/download/v2.0.0/gardenctl_v2_linux_amd64"
     sha256 "85d728ab5653553e7243f8fd7955ff5d4502b9b79a8a6ea24440d62ebd9b1228"
+    depends_on :arch => :x86_64
   end
-
-  depends_on :arch => :x86_64
 
   def install
     bin.install stable.url.split("/")[-1] => "gardenctl"
