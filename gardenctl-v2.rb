4282af15158f792167aae3b7dd50126a7c6bea82
gardener-robot-ci-1 <gardener.ci.user@gmail.com>
2023-01-25 11:15:51 +0000

    Update gardenctl-v2

--- gardenctl-v2.rb
+++ gardenctl-v2.rb
@@ -1,21 +1,21 @@
 class GardenctlV2 < Formula
   desc "Gardenctl-v2"
   homepage "https://gardener.cloud"
-  version "v3.0.8"
+  version "v3.0.9"
 
   depends_on "gardener/tap/gardenlogin"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/gardener/gardenctl-v2/releases/download/v3.0.8/gardenctl_v2_darwin_arm64"
-      sha256 "4642f5117d5bf2b5da481330e7d1d75c2cfb537a2e9c46d40f9206c0ea0ee645"
+      url "https://github.com/gardener/gardenctl-v2/releases/download/v3.0.9/gardenctl_v2_darwin_arm64"
+      sha256 "abc"
     else
-      url "https://github.com/gardener/gardenctl-v2/releases/download/v3.0.8/gardenctl_v2_darwin_amd64"
-      sha256 "543e8c8e34d2db5060ee3ac1b82ea468ebd64cb26dc60d196b294c8444b9e2e6"
+      url "https://github.com/gardener/gardenctl-v2/releases/download/v3.0.9/gardenctl_v2_darwin_amd64"
+      sha256 "abc"
     end
   elsif OS.linux?
-    url "https://github.com/gardener/gardenctl-v2/releases/download/v3.0.8/gardenctl_v2_linux_amd64"
-    sha256 "58dc9858f6ea87d976781d48be70b469a1a2d73652a4688289509dff60d40c91"
+    url "https://github.com/gardener/gardenctl-v2/releases/download/v3.0.9/gardenctl_v2_linux_amd64"
+    sha256 "anc"
     depends_on :arch => :x86_64
   end
 
