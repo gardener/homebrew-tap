ded17d41144cec705766dcf2a0ccd4ca18037249
Peter Sutter <peter.sutter@sap.com>
2023-12-04 13:07:02 +0100

    support linux/arm64 (#39)

--- .github/workflows/update-gardenlogin.sh
+++ .github/workflows/update-gardenlogin.sh
@@ -4,6 +4,7 @@ tag=${1}
 darwin_sha_amd64=${2}
 darwin_sha_arm64=${3}
 linux_sha_amd64=${4}
+linux_sha_arm64=${5}
 if [ -z "${1}" ]
 then
       echo "release tag is not provided"
@@ -21,7 +22,12 @@ then
 fi
 if [ -z "${4}" ]
 then
-      echo "linux binary sha256sum is not provided"
+      echo "linux amd64 binary sha256sum is not provided"
+      exit 1
+fi
+if [ -z "${5}" ]
+then
+      echo "linux arm64 binary sha256sum is not provided"
       exit 1
 fi
 
@@ -29,6 +35,7 @@ echo $tag
 echo $darwin_sha_amd64
 echo $darwin_sha_arm64
 echo $linux_sha_amd64
+echo $linux_sha_arm64
 
 cat > gardenlogin.rb << EOF
 class Gardenlogin < Formula
@@ -45,9 +52,14 @@ class Gardenlogin < Formula
       sha256 "$darwin_sha_amd64"
     end
   elsif OS.linux?
-    url "https://github.com/gardener/gardenlogin/releases/download/$tag/gardenlogin_linux_amd64"
-    sha256 "$linux_sha_amd64"
-    depends_on :arch => :x86_64
+    if Hardware::CPU.arm?
+      url "https://github.com/gardener/gardenlogin/releases/download/$tag/gardenlogin_linux_arm64"
+      sha256 "$linux_sha_arm64"
+    else
+      url "https://github.com/gardener/gardenlogin/releases/download/$tag/gardenlogin_linux_amd64"
+      sha256 "$linux_sha_amd64"
+      depends_on :arch => :x86_64
+    end
   end
 
   def install
